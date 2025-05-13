12422a6024875b86ffd92ce4135233b6a8213e6f
victor loginov <tkoe88@zoho.com>
2025-05-13 23:40:04 +0200

    updated script to create better slug routes

--- 01-essentials/app/meals/[slug]/page.tsx
+++ 01-essentials/app/meals/[slug]/page.tsx
@@ -1,9 +1,12 @@
 import styles from './page.module.css';
 
-export default function MealsSlug() {
+// params is now aync, for the Functional component must be async
+export default async function MealsSlug({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   return (
     <div className={styles['meals-slug']}>
       <h1>MealsSlug page</h1>
+      <p>ID: {slug}</p>
     </div>
   );
 }
--- 01-essentials/create-route.mjs
+++ 01-essentials/create-route.mjs
@@ -1,6 +1,27 @@
 import fs from 'fs';
 import path from 'path';
 
+// 0: Node, 1: Script, 2: args (anything written in CLI after script)
+const routeName = process.argv[2]?.toLowerCase();
+if (!routeName) {
+  console.error('❌ Route name required');
+  process.exit(1);
+}
+// path/slug becomes path/[slug] automatically when dir name ends with 'slug'
+const dir = path.join('app', routeName).replace(/slug$/, '[slug]');
+if (fs.existsSync(dir)) {
+  console.error('❌ Route already exists');
+  process.exit(1);
+}
+
+let [async, params, slug, slugId] = ['', '', '', ''];
+if (routeName.endsWith('slug')) {
+   async = 'async '; // with exact spacing for correct print
+  params = '{\n  params,\n}: {\n  params: Promise<{ slug: string }>;\n}';
+    slug = '\n  const { slug } = await params;';
+  slugId = '\n      <p>ID: {slug}</p>';
+}
+
 function pascalCase(str) {
   return str
     .split(/[-/]/) // Split on '-' or '/'
@@ -12,33 +33,18 @@ function kebabCase(str) {
   return str.replace(/[_/]/g, '-'); // replace all _ / with -
 }
 
-// 0: Node, 1: Script, 2: args (anything written in CLI after script)
-const routeName = process.argv[2]?.toLowerCase();
-if (!routeName) {
-  console.error('❌ Route name required');
-  process.exit(1);
-}
-
-// path/slug becomes path/[slug] automatically when dir name ends with 'slug'
-const      dir = path.join('app', routeName).replace(/slug$/, '[slug]');
-const pagePath = path.join(dir, 'page.tsx');
-const  cssPath = path.join(dir, 'page.module.css');
-
-if (fs.existsSync(dir)) {
-  console.error('❌ Route already exists');
-  process.exit(1);
-}
-
+const  pagePath = path.join(dir, 'page.tsx');
+const   cssPath = path.join(dir, 'page.module.css');
 const className = kebabCase(routeName);
 const component = pascalCase(routeName);
 fs.mkdirSync(dir, { recursive: true });
 fs.writeFileSync(cssPath, `.${className} {\n  padding: 1rem;\n}\n`);
 fs.writeFileSync(pagePath, `import styles from './page.module.css';
 
-export default function ${component}() {
+export default ${async}function ${component}(${params}) {${slug}
   return (
     <div className={styles['${className}']}>
-      <h1>${component} page</h1>
+      <h1>${component} page</h1>${slugId}
     </div>
   );
 }
