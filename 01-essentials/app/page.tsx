5dc4369112b3f99fa957324e0213c633c1dd80f6
victor loginov <tkoe88@zoho.com>
2025-05-14 03:24:14 +0200

    added main page content

--- 01-essentials/app/page.tsx
+++ 01-essentials/app/page.tsx
@@ -1,9 +1,57 @@
+import Link from 'next/link';
 import styles from './page.module.css';
+
+const Section = ({
+    children,
+  paragraphs,
+}: {
+    children: React.ReactNode;
+  paragraphs: string[];
+}) => (
+  <section className={styles.section}>
+    <h2>{children}</h2>
+    {paragraphs.map((p, i) => (
+      <p key={i}>{p}</p>
+    ))}
+  </section>
+);
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      {'Whereas recognition of the inherent dignity'.repeat(10)}
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.slideshow} />
+        <div>
+          <div className={styles.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href='/community'>Join the Community</Link>
+            <Link href='/meals'>Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Section
+          paragraphs={[
+            "NextLevel Food is a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes, and to connect with other food lovers.",
+
+            "NextLevel Food is a place to discover new dishes, and to connect with other food lovers.",
+          ]}
+        >
+          How it works
+        </Section>
+        <Section
+          paragraphs={[
+            "NextLevel Food is a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes, and to connect with other food lovers.",
+
+            "NextLevel Food is a place to discover new dishes, and to connect with other food lovers.",
+          ]}
+        >
+          Why NextLevel Food?
+        </Section>
+      </main>
+    </>
   );
 }
