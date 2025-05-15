bf3223078d2f9e30725e1fd68713a818ed29213c
victor loginov <tkoe88@zoho.com>
2025-05-15 23:48:11 +0200

    added meals share form & input component

--- 01-essentials/app/meals/share/page.tsx
+++ 01-essentials/app/meals/share/page.tsx
@@ -1,9 +1,27 @@
+import Input from '@/components/form/Input';
 import styles from './page.module.css';
 
 export default function MealsShare() {
   return (
     <div className={styles['meals-share']}>
-      <h1>MealsShare page</h1>
+      <header>
+        <h1>
+          Share your <span className='highlight'>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <form className={styles['form']}>
+        <div className={styles['row']}>
+          <Input control='name'>Your name</Input>
+          <Input control='email'>Your email</Input>
+        </div>
+        <Input control='title'>Title</Input>
+        <Input control='summary'>Short Summary</Input>
+        <Input control='instructions' rows={10}>
+          Instructions
+        </Input>
+        <button>Share Meal</button>
+      </form>
     </div>
   );
 }
--- 01-essentials/components/form/Input.tsx
+++ 01-essentials/components/form/Input.tsx
@@ -1,9 +1,26 @@
 import styles from './Input.module.css';
 
-export default function Input() {
+export default function Input({
+   control,
+      rows,
+      type = 'text',
+  children,
+}: {
+   control: string;
+     rows?: number;
+     type?: 'text' | 'number' | 'email' | 'password';
+  children: React.ReactNode;
+}) {
+  let input = <input type={type} id={control} name={control} required />;
+
+  if (rows) {
+    input = <textarea id={control} name={control} rows={rows} required />;
+  }
+
   return (
-    <div className={styles['input']}>
-      <h1>Input created</h1>
-    </div>
+    <p className={styles['control']}>
+      <label htmlFor={control}>{children}</label>
+      {input}
+    </p>
   );
 }
