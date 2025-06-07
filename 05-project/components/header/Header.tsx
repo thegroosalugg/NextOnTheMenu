736f1c91185d066856b048f026b117119b809c6a
victor loginov <tkoe88@zoho.com>
2025-06-07 23:00:39 +0200

    updated header to tailwind

--- 05-project/components/header/Header.tsx
+++ 05-project/components/header/Header.tsx
@@ -1,17 +1,23 @@
-import styles from './Header.module.css';
 import Svg from '../ui/icon/Svg';
 import NavLink from './NavLink';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <NavLink href='/'>
-        <Svg icon='Shield' />
-        <span>Auth</span>
-      </NavLink>
-      <nav>
+    <header className='flex items-center justify-between gap-1 overflow-x-scroll p-2'>
+      <nav className='flex items-center gap-1 font-mono'>
+        <Link href='/' className='flex items-center font-sans'>
+          <Svg icon='Shield' />
+          <span>Shopify</span>
+        </Link>
+        <NavLink href='/'>Link</NavLink>
+        <NavLink href='/'>Link</NavLink>
+        <NavLink href='/'>Link</NavLink>
         <NavLink href='/'>Link</NavLink>
       </nav>
+      <button>
+        Cart
+      </button>
     </header>
   );
 }
--- 05-project/components/header/NavLink.tsx
+++ 05-project/components/header/NavLink.tsx
@@ -10,10 +10,11 @@ export default function NavLink({
   children: React.ReactNode;
 }) {
   const path = usePathname();
-  const isActive = path.startsWith(href) && href.length > 1 ? 'highlight' : '';
+  let classes = 'transition-all hover:text-shadow-md text-shadow-sky-500';
+  if (path.startsWith(href) && href.length > 1) classes += ' text-sky-600';
 
   return (
-    <Link href={href} className={isActive}>
+    <Link href={href} className={classes}>
       {children}
     </Link>
   );
