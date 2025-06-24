9e15b1be4459bfbfa7800d1f1f460441b50c53cf
victor loginov <tkoe88@zoho.com>
2025-06-24 04:35:39 +0200

    added caching function wrapper

--- 05-project/lib/actions/product.ts
+++ 05-project/lib/actions/product.ts
@@ -3,10 +3,22 @@ import Product from "@/model/product";
 import { unstable_cache } from "next/cache";
 import { cache } from "react";
 
-export async function getProducts() {}
+interface CacheOptions {
+  revalidate?: number;
+        tags?: string[];
+}
 
-export const findProduct = unstable_cache(
-  cache(async function findProduct(_id: string) {
-    return await Product.findById(_id);
-  })
+function cached<Args extends unknown[], T>(
+  fetchData: (...args: Args) => Promise<T>,
+   keyParts: string[],
+    options: CacheOptions = {}
+) {
+  return unstable_cache(cache(fetchData), keyParts, options);
+}
+
+export const findProduct = cached(
+  async (_id: string) => await Product.findById(_id),
+  ["product"]
 );
+
+export async function getProducts() {}
