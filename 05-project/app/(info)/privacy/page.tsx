c05a2abb723e4fe0c1f3e3ed17ffb3c22a98d4e6
victor loginov <tkoe88@zoho.com>
2026-01-31 21:24:20 +0100

    privcacy page metadata added

--- 05-project/app/(info)/privacy/page.tsx
+++ 05-project/app/(info)/privacy/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+        title: 'Privacy | Shopify',
+  description: 'Privacy Policy',
+};
+
 export default function PrivacyPolicyPage() {
   return (
     <div className="max-w-2xl mx-auto p-4 wrap-break-word">
